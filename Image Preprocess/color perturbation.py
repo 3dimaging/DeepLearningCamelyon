d02301c93573df1e74e8388d2f636c392a98d7f2
Weizhe Li <weizheli@gmail.com>
2018-09-03 19:45:16 -0400

    Update color perturbation.py

--- Image Preprocess/color perturbation.py
+++ Image Preprocess/color perturbation.py
@@ -1,12 +1,23 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-def color_pertubation(image):
+def color_perturbation(image):
       
       image = tf.image.random_brightness(image, max_delta=64./ 255.)
-      image = tf.image.random_saturation(image, max_delta=0.25)
+      image = tf.image.random_saturation(image, lower=0, upper=0.25)
       image = tf.image.random_hue(image, max_delta=0.04)
-      image = tf.image.random_contrast(image, max_delta=0.75)
+      image = tf.image.random_contrast(image, lower=0, upper=0.75)
+      
       
       
       
       return image
+
+
+image_color_perturb = color_perturbation(image)
+
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+im = sess.run(image_color_perturb)
+plt.imshow(npf)
